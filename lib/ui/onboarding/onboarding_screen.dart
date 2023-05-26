8d43fa082ca8327c7bf0d67b4d609d58f63ef354
CodePhilanthropist <barrigaete@gmail.com>
2023-05-26 08:25:26 +0800

    partially deploy onboarding

--- lib/ui/onboarding/onboarding_screen.dart
+++ lib/ui/onboarding/onboarding_screen.dart
@@ -1,26 +1,155 @@
 import 'package:flutter/material.dart';
 import 'package:walleto/utils/colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import "../../utils/buttons.dart";
 
 class OnboardingScreen extends StatelessWidget {
-  const OnboardingScreen({super.key});
+  const OnboardingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: AppColor.navyPurple,
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 85),
-            child: Stack(
-              children: [
-                SvgPicture.asset("assets/onboarding/landing-image.svg"),
-                SvgPicture.asset("assets/onboarding/stars.svg"),
-              ],
-            ),
+      body: SafeArea(
+        child: SizedBox(
+          width: deviceWidth,
+          height: deviceHeight,
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      child: Image.asset(
+                          "assets/images/onboarding/landing-image.png"),
+                    ),
+                    Positioned(
+                      child: SvgPicture.asset(
+                          "assets/images/onboarding/stars.svg"),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text.rich(
+                            TextSpan(
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'Make Payments Much\n',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 34,
+                                    fontFamily: "Avenir",
+                                    height: 1.2,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: 'Easier In ',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 34,
+                                    fontFamily: "Avenir",
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: 'Once Place',
+                                  style: TextStyle(
+                                    color: AppColor.lightOrange,
+                                    fontSize: 34,
+                                    fontFamily: "Avenir",
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            "Your Money Is Safe & Growing With Us.",
+                            style: TextStyle(
+                              fontFamily: "Avenir",
+                              color: Colors.white,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 27,
+                    ),
+                    SizedBox(
+                      child: Row(
+                        children: [
+                          const GradientButton(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xffFB9F6C),
+                                Color(0xffC66CFC),
+                                Color(0xff8247FF),
+                              ],
+                            ),
+                            borderGradient: RadialGradient(colors: [
+                              Color(0xffFB9F6C),
+                              Color(0xffC66CFC),
+                              Color(0xff8247FF),
+                            ]),
+                            radius: 16,
+                            strokeWidth: 1,
+                            child: Text(
+                              "Get Started",
+                              style: TextStyle(
+                                fontFamily: "Avenir",
+                                fontSize: 17,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 30,
+                          ),
+                          GestureDetector(
+                            child: Row(
+                              children: [
+                                const Text(
+                                  "Skip",
+                                  style: TextStyle(
+                                    fontFamily: "Avenir",
+                                    fontSize: 17,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SvgPicture.asset(
+                                  "assets/images/onboarding/arrow-right.svg",
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
---
+++ lib/utils/buttons.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class GradientButton extends StatelessWidget {
+  final Widget child;
+  final Gradient gradient;
+  final double strokeWidth;
+  final double radius;
+  final Gradient borderGradient;
+
+  const GradientButton({
+    Key? key,
+    required this.child,
+    required this.gradient,
+    this.strokeWidth = 2.0,
+    this.radius = 4.0,
+    required this.borderGradient,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 245,
+      height: 56,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        gradient: borderGradient,
+      ),
+      padding: EdgeInsets.all(strokeWidth),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(radius),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {},
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: gradient,
+              ),
+              child: Center(
+                child: child,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
